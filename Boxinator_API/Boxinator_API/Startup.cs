2038a89e4a5e492e024305a5a5e377a0d7f78f86
Miltownee <72917466+Miltownee@users.noreply.github.com>
2022-09-07 12:51:25 +0200

    feat: uncomment out swagger in startup

--- Boxinator_API/Boxinator_API/Startup.cs
+++ Boxinator_API/Boxinator_API/Startup.cs
@@ -34,17 +34,17 @@ namespace Boxinator_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BoxApiDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo
-            //    {
-            //        Title = "Boxinator_API",
-            //        Version = "v1"
-            //    });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Boxinator_API",
+                    Version = "v1"
+                });
                 //    //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 //    //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 //    //c.IncludeXmlComments(xmlPath);
-            //});
+            });
             services.AddScoped<IGuestUserRepository, GuestUserRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IShipmentRepository, ShipmentRepository>();
@@ -72,8 +72,8 @@ namespace Boxinator_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Boxinator_API v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Boxinator_API v1"));
             }
 
             app.UseHttpsRedirection();
